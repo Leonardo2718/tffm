6e967023894a4c70ed36d51025222ad1a75401a3
Leonardo Banderali <leonardo2718@protonmail.com>
2016-09-11 15:49:27 -0400

    Initial commit

---
+++ src/mainwindow.hpp
@@ -0,0 +1,52 @@
+/*
+Project: tffm
+Author: Leonardo Banderali
+
+Description:
+    tffm is a simple, keyboard-centric file manager intended for use with tiling
+    window managers such as Xmonad.
+
+License:
+
+    MIT License
+
+    Copyright (c) 2016 Leonardo Banderali
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#ifndef MAINWINDOW_HPP
+#define MAINWINDOW_HPP
+
+#include <QMainWindow>
+
+namespace tffm { class MainWindow; }
+
+class tffm::MainWindow : public QMainWindow {
+        Q_OBJECT
+
+    public:
+        explicit MainWindow(QWidget *parent = nullptr);
+
+    signals:
+
+    public slots:
+};
+
+#endif // MAINWINDOW_HPP
